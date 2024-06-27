1e8e4c3954c4da5bef844803b2f32f85ca53de4e
Azara <yyushahu02@gmail.com>
2024-06-27 02:53:42 +0000

    refactor code. include returns and status strings

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,12 +4,16 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
     .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => {
-      results.map((result) => ({
-        return: {
-          status: result.status,
-          value: result.value || result.reason,
-        },
-      }));
-    });
+    .then((results) => results.map((result) => {
+      if (result.status === 'fulfilled') {
+        return {
+          status: 'fulfilled',
+          value: result.value,
+        };
+      }
+      return {
+        status: 'rejected',
+        value: result.reason,
+      };
+    }));
 }
