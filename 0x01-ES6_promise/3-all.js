2577e5850c6d1a4512505f6c5750da1d4f59aeba
Azara <yyushahu02@gmail.com>
2024-06-27 01:26:09 +0000

    handle multiple successful promises using all, then, and catch

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((responses) => {
+      const photo = responses[0];
+      const user = responses[1];
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
