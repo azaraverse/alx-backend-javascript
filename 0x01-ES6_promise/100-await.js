c98a5003961aa20dd16b7ae397a9547b6b38f9ba
Azara <yyushahu02@gmail.com>
2024-06-27 04:08:19 +0000

    refactor code and catch an error on failure of one async function. return required response on failure

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,18 +1,19 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-  const responses = await Promise
-    .all([uploadPhoto(), createUser()]);
-  const photo = responses[0];
-  const user = responses[1];
-  return {
-    photo: {
-      status: photo.status,
-      body: photo.body,
-    },
-    user: {
-      firstName: user.firstName,
-      lastName: user.lastName,
-    },
-  };
+  try {
+    const responses = await Promise
+      .all([uploadPhoto(), createUser()]);
+    const photo = responses[0];
+    const user = responses[1];
+    return {
+      photo,
+      user,
+    };
+  } catch (error) {
+    return {
+      photo: null,
+      user: null,
+    };
+  }
 }
