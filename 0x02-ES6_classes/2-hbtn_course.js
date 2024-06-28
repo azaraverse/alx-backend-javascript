a0ec523cbd84fc49fae6c89c0f83c641f5d2d722
Azara <yyushahu02@gmail.com>
2024-06-28 15:04:32 +0000

    using getters and setters in classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,53 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof name === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+    if (typeof length === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+    if (Array.isArray(students) && students.every((student) => typeof student === 'string')) {
+      this._students = students;
+    } else {
+      throw new TypeError('Students must be an array of strings');
+    }
+  }
+
+  // getter for name attribute
+  get name() {
+    return this._name;
+  }
+
+  // setter for name attribute
+  set name(value) {
+    if (typeof value === 'string') {
+      this._name = value;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  // getter for length attribute
+  get length() {
+    return this._length;
+  }
+
+  // setter for length attribute
+  set length(value) {
+    this._length = value;
+  }
+
+  // getter for students attribute
+  get students() {
+    return this._students;
+  }
+
+  // setter for students attribute
+  set students(value) {
+    this._students = value;
+  }
+}
