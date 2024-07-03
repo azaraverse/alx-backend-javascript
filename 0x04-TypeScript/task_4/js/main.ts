8379cc2a76b43b607c0e077cb75be0af64c6471e
Azara <yyushahu02@gmail.com>
2024-07-03 23:57:31 +0000

    more namespaces and declaration merging

---
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -0,0 +1,30 @@
+/// <reference path="subjects/Teacher.ts" />
+/// <reference path="subjects/Subject.ts" />
+
+export const cpp: Subjects.Cpp = new Subjects.Cpp();
+export const java: Subjects.Java = new Subjects.Java();
+export const react: Subjects.React = new Subjects.React();
+
+export const cTeacher: Subjects.Teacher = {
+    firstName: 'Sir Balo',
+    lastName: 'Sabinus',
+    experienceTeachingC: 10,
+};
+
+// Cpp subject
+console.log('C++');
+cpp.setTeacher(cTeacher);
+console.log(cpp.getRequirements());
+console.log(cpp.getAvailableTeacher());
+
+// Java subject
+console.log('Java');
+java.setTeacher(cTeacher);
+console.log(java.getRequirements());
+console.log(java.getAvailableTeacher());
+
+// React subject
+console.log('React');
+react.setTeacher(cTeacher);
+console.log(react.getRequirements());
+console.log(react.getAvailableTeacher());
---
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -0,0 +1,10 @@
+namespace Subjects {
+  export class Subject {
+    teacher: Teacher;
+
+    // setter method
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    };
+  }
+}
