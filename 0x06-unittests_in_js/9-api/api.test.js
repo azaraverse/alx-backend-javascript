c56bfff90873f2ba382c060aa412a85ff17d7667
Azara <yyushahu02@gmail.com>
2024-08-22 21:16:13 +0000

    integrate regex matching to get id number strictly

---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,25 @@
+const request = require('request');
+const { expect } = require('chai');
+
+describe('GET index page', () => {
+  it('responds with required information', () => {
+    request('http://localhost:7865', (req, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+    });
+  });
+});
+
+describe('correct status code for when id is a number', () => {
+  it('checks for valid id number', () => {
+    request('http://localhost:7865/cart/10', (req, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 10');
+    });
+  });
+  it('checks for invalid id number', () => {
+    request('http://localhost:7865/cart/abc', (req, res, body) => {
+      expect(res.statusCode).to.equal(404);
+    });
+  });
+});
