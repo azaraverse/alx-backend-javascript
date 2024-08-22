09b3f655715851d3e4a5110c7749a9657d0e9e19
Azara <yyushahu02@gmail.com>
2024-08-22 16:34:37 +0000

    finalise using chai for writing tests for readability

---
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable jest/expect-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/valid-expect */
+const chai = require('chai');
+const calculateNumber = require('./1-calcul');
+
+const { expect } = chai;
+
+describe('calculateNumber', () => {
+  it('return sum of two round numbers', () => {
+    expect(calculateNumber('SUM', 1.3, 2.6)).to.equal(4);
+  });
+  it('return difference of one number from another round number', () => {
+    expect(calculateNumber('SUBTRACT', 10, 5.5)).to.equal(4);
+  });
+  it('return quotient of one number and another round number', () => {
+    expect(calculateNumber('DIVIDE', 6, 2.3)).to.equal(3);
+  });
+  it('return quotient of two zero numbers', () => {
+    expect(calculateNumber('DIVIDE', 0.4, 0.1)).to.equal('Error');
+  });
+});
