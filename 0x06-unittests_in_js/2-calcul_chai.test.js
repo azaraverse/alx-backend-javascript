6a059ced4753aecc316396cbcbc5f6ee74cbc382
Azara <yyushahu02@gmail.com>
2024-08-22 16:37:07 +0000

    change import location of function

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,7 +2,7 @@
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/valid-expect */
 const chai = require('chai');
-const calculateNumber = require('./1-calcul');
+const calculateNumber = require('./2-calcul_chai');
 
 const { expect } = chai;
 
