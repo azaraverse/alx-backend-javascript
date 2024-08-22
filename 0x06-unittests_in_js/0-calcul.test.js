96630f2100814ae4554db40c0069d49dd82f9fe6
Azara <yyushahu02@gmail.com>
2024-08-22 16:17:24 +0000

    fix bugs in test case

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -5,13 +5,13 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('return sum of two floored numbers', () => {
-    assert.equal(calculateNumber(1.3, 2.6), 3);
+    assert.equal(calculateNumber(1.3, 2.6), 4);
   });
   it('return sum of one number and another floored number', () => {
-    assert.equal(calculateNumber(10, 5.5), 15);
+    assert.equal(calculateNumber(10, 5.5), 16);
   });
   it('return sum of one negative number and another floored number', () => {
-    assert.equal(calculateNumber(-2.3, 4.3), 1);
+    assert.equal(calculateNumber(-2.3, 4.3), 2);
   });
   it('return sum of two zero numbers', () => {
     assert.equal(calculateNumber(0.4, 0.1), 0);
